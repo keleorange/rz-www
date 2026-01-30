89b9472ef07fb33bca24ed77ae3439cd8c627925
horizon0514 <horizon0514@gmail.com>
2026-01-30 12:47:53 +0800

    add migrations

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260130_044719 from './20260130_044719';
+
+export const migrations = [
+  {
+    up: migration_20260130_044719.up,
+    down: migration_20260130_044719.down,
+    name: '20260130_044719'
+  },
+];
